d805c9b713376fa47c5d0c1873f3f6c083f096d5
Hann <launchinghann@facebook.com>
2012-12-31 18:24:46 +0900

    edit register comfirm code

--- public/javascripts/register.js
+++ public/javascripts/register.js
@@ -1,5 +1,5 @@
 // map
-var clickedMarker;
+var clickedMarker = null;
 var placeMarker = function(location){
     console.log(clickedMarker);
     console.log(location);
@@ -42,17 +42,27 @@ $(document).ready(function() {
 })
 
 $('#regist').click(function() {
-    var position = clickedMarker.getPosition();
-    var lat = position.Ya;
-    var lng = position.Za;
-    $('#lat').val(lat);
-    $('#lng').val(lng);
+    var nameValid = $('#name').val() !== "";
+    var placeValid = $('#place').val() !== "";
+
     if (confirm("확인해주세요. \n\n" +
         "1. 지도에 마커는 올바르게 표시했는지. \n" +
         "2. 이름은 잘 작성했는지. \n" +
         "3. 설명은 누가봐도 이해하도록 작성했는지. \n\n" +
         "모두 다 맞게 쓰셨다면 확인을 눌러주세요 :)")){
-        $('form').submit();
+        if (!clickedMarker && nameValid && placeValid){
+            var position = clickedMarker.getPosition();
+            var lat = position.Ya;
+            var lng = position.Za;
+
+            $('#lat').val(lat);
+            $('#lng').val(lng);
+            $('form').submit();
+        }
+        else {
+            alert("거짓말쟁이...");
+            return false;
+        }
     }
     else {
         return false;
